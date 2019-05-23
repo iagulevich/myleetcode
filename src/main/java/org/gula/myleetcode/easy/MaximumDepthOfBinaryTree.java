1fbabf1ebefbdf6497ca0df9ca69b6d71f557e4e
Ihar Hulevich <iagulevich@gmail.com>
2019-05-23 16:01:19 -0700

    +easy, without ide

---
+++ src/main/java/org/gula/myleetcode/easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,21 @@
+package org.gula.myleetcode.easy;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        return Math.max(l, r) + 1;
+    }
+}
