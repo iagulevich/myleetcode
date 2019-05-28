0b370f7548b5aba191235899c0456cf0767442d9
Ihar Hulevich <iagulevich@gmail.com>
2019-05-28 11:48:11 -0700

    +easy, did not solve, but learn algorithm

---
+++ src/main/java/org/gula/myleetcode/easy/CountPrimes.java
@@ -0,0 +1,32 @@
+package org.gula.myleetcode.easy;
+
+public class CountPrimes {
+    public int countPrimes(int n) {
+        boolean[] bool = new boolean[n];
+
+        for (int i = 0; i< bool.length; i++) {
+            bool[i] = true;
+        }
+        for (int i = 2; i< Math.sqrt(n); i++) {
+            if(bool[i] == true) {
+                for(int j = (i*i); j<n; j = j+i) {
+                    bool[j] = false;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 2; i< bool.length; i++) {
+            if(bool[i]==true) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result = new CountPrimes().countPrimes(30);
+        System.out.println(result);
+
+    }
+}
