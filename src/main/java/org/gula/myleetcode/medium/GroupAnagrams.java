40613ca44456e582d8d49cc3a32217fb5c6a6ab7
Ihar Hulevich <iagulevich@gmail.com>
2019-06-25 11:52:39 -0700

    + medium - without ide, not best solution

---
+++ src/main/java/org/gula/myleetcode/medium/GroupAnagrams.java
@@ -0,0 +1,21 @@
+package org.gula.myleetcode.medium;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
