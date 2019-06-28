d29097e99b0b5fae3d091a63d3385679eea113a5
Ihar Hulevich <iagulevich@gmail.com>
2019-06-28 12:18:28 -0700

    + easy - not best solution

---
+++ src/main/java/org/gula/myleetcode/easy/RotateArray.java
@@ -0,0 +1,21 @@
+package org.gula.myleetcode.easy;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        while (k > 0) {
+            int temp = nums[n - 1];
+            for (int i = n - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = temp;
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+        new RotateArray().rotate(new int[]{1, 2, 3, 4, 5}, 2);
+    }
+}
